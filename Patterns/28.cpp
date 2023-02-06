0da7f71fa0a4604e42f25e4852f54ed45cec86a5
Shreya-bansall <shreyab2400@gmail.com>
2023-02-06 21:25:55 +0530

    Pattern

--- Patterns/28.cpp
+++ Patterns/28.cpp
@@ -7,63 +7,6 @@
 #include<iostream>
 using namespace std;
 
-// int main(){
-//     int n=5;
-
-//     for(int i=1;i<=n;i++){
-//         for(int j=1;j<=n;j++){
-//             if(j<=i){
-//                 cout<<j;
-//             }
-//             else{
-//                 cout<<"*";
-//             }
-//         }
-//         cout<<"\n";
-//     }
-
-//     return 0;
-// }
-
-/*int main(){
-    int n=5;
-
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-                cout<<j;
-        }
-        for(int j=1;j<(2*i-1);j++){
-            cout<<"*";
-        }
-        for(int j=i;j>=1;j--){
-                cout<<j;
-        }
-        cout<<"\n";
-    }
-
-    return 0;
-}*/
-
-
-    // int main(){
-    // int n=5;
-
-    // for(int i=1;i<=n;i++){
-    //     for(int j=1;j<=i;j++){
-    //             cout<<j;
-    //     }
-    //     for(int j=(n-1)*2;j>0;j){
-    //         cout<<"*";
-    //     }
-    //     for(int j=i;j>=1;j--){
-    //             cout<<j;
-    //     }
-    //     cout<<"\n";
-//     // }
-
-//     return 0;
-// }
-
 int main(){
     int n=5;
 
