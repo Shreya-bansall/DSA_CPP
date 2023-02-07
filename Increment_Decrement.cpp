35816aae0255c90bc8968f43f1616a4083031460
Shreya-bansall <shreyab2400@gmail.com>
2023-02-07 13:47:44 +0530

    Increment & Decrement Operator

---
+++ Increment_Decrement.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=7;
+
+    cout<<(++i)<<"\n";
+    cout<<(i++)<<"\n";
+    cout<<(i--)<<"\n";
+    cout<<(--i);
+
+    return 0;
+}
