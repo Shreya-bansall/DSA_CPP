2a76b71282d1ca04dbeb5eb1e78bcb77e9f83945
Shreya-bansall <shreyab2400@gmail.com>
2023-02-09 18:36:56 +0530

    Decimal To Binary (Neagative)

---
+++ Conversion/NegativeDecimalToBinary.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n=-6, i=0,ans=0;
+    // Convert into positive number
+    n=n+(-2*n);
+    
+    // Convert into Binary
+    while(n!=0){
+        int bit=n&1;
+        ans = (bit*pow(10,i))+ans;
+        n=n>>1;
+        i++;
+    }
+
+    // Take 1's compliment
+    ans=~ans;
+    // Take 2's Compliment 
+    ans=ans+1;
+    cout<<ans;
+
+    return 0;
+}
