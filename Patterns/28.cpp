3fc6f3935ef03e36476c93d450e9b683ded182e9
Shreya-bansall <shreyab2400@gmail.com>
2023-02-13 12:03:45 +0530

    Pattern

--- Patterns/28.cpp
+++ Patterns/28.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main(){
     int n=5;
 
-    for(int i=1;i<=n;i++){
+    for(int i=n;i>=1;i--){
         for(int j=1;j<=n;j++){
             if(j<=i){
                 cout<<j;
