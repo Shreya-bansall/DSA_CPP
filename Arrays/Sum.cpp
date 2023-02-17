40141a65aeec426c0e4a34ccd12491c8b6a26568
Shreya-bansall <shreyab2400@gmail.com>
2023-02-17 10:29:41 +0530

    Sum of all elements in an array

---
+++ Arrays/Sum.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int sum(int arr[], int n){
+    int sum=0;
+
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+    }
+
+    return sum;
+}
+
+int main(){
+    int size;
+    cout<<"Enter size of array: ";
+    cin>>size;
+    int arr[100];
+    cout<<"Enter array elements: ";
+
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+
+    cout<<"Sum of all elements in an array: "<<sum(arr,size);
+
+    return 0;
+}
