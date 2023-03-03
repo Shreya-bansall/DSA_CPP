5eb26112193e1b796bbb430d5ca97af49ea9812f
Shreya-bansall <shreyab2400@gmail.com>
2023-03-03 13:36:43 +0530

    Unique Number of Occurrences

---
+++ LeetCode Questions/6.cpp
@@ -0,0 +1,22 @@
+// Link: - https://leetcode.com/problems/unique-number-of-occurrences/
+// Unique Number of Occurrences
+
+#include<iostream>
+using namespace std;
+
+int UniqueOccurence(int arr[], int n){
+    
+}
+
+int main(){
+    int size;
+    cout<<"Enter size of array: ";
+    cin>>size;
+    int arr[100];
+
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+
+    return 0;
+}
