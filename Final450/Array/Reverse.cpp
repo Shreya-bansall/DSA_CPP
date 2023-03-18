0c83c44c5c15a85efcbf1df136c89ca5585f3dcb
Shreya-bansall <shreyab2400@gmail.com>
2023-03-18 19:29:58 +0530

    Reverse

--- Final450/Array/Reverse.cpp
+++ Final450/Array/Reverse.cpp
@@ -6,7 +6,7 @@ int main(){
     cout<<"Enter size of array: ";
     int arr[100];
     cout<<"Enter elements of an array: ";
-    cout<<"Reverse: ";
+    cout<<"Reverse of a number: ";
 
     return 0;
 }
