5f4779046eadc9d89e2f1126c288fdaab9c1c7fb
Shreya-bansall <shreyab2400@gmail.com>
2023-07-06 22:29:26 +0530

    Reverse a String

---
+++ LeetCode Questions/15.cpp
@@ -0,0 +1,37 @@
+// Link: - https://leetcode.com/problems/reverse-string/
+// Reverse a String
+
+#include<iostream>
+using namespace std;
+
+void Reverse(char name[], int n){
+    int s=0;
+    int e=n-1;
+
+    while(s<=e){
+        swap(name[s++],name[e--]);
+    }
+}
+
+int GetLength(char name[]){
+    int count=0;
+    for(int i=0;name[i]!='\0';i++){
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    char name[20];
+    cout<<"Enter your name: ";
+    cin>>name;
+
+    int a=GetLength(name);
+
+    Reverse(name,a);
+
+    cout<<"Your name after reverse: ";
+    cout<<name;
+
+    return 0;
+}
